b7bac585479b8eeb5ee2c94963aac0acdbdf5cee
Melissa Lambert <m.b.lambert@live.com>
2017-12-08 13:35:12 -0500

    attempting merge conflict again

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //JS File
 (function(){
 	console.log('Hello, I am trying to create a merge conflict!');
-	console.log('creating another merge conflict to try and figure this thing out'); //kept my changes made locally and removed line pulled from remote(2nd attempt).
+	console.log('Still trying to create a merge conflict'); //kept my changes made locally and removed line pulled from remote(2nd attempt).
 })();
