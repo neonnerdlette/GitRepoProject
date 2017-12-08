1a31a5ef5d45ceb5a8f072f64d5aba7f406f39ed
neonnerdlette <33646750+neonnerdlette@users.noreply.github.com>
2017-12-08 13:21:42 -0500

    Update main.js
    trying to figure this merge conflict thing out...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //JS File
 (function(){
 	console.log('Hello, I am trying to create a merge conflict!');
-	console.log('creating another merge conflict'); //kept my changes made locally and removed line pulled from remote.
+	console.log('Making changes again'); //kept my changes made locally and removed line pulled from remote.
 })();
